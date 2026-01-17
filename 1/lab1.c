01800a84476603df0c8eea812ecda9c7eceab7fc
Tong228 <KKKostya55@mail.ru>
2026-01-17 15:21:44 +1000

    1 lab well done

---
+++ 1/lab1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+    double a = 1, b = -2, c = 1;
+    double d;
+    double x1, x2;
+
+    d = pow(b, 2) - 4 * a * c;
+    /*printf("%f", d);*/
+
+    if(d>0)
+    {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        printf("x1 = %f\nx2 = %f", x1, x2);
+    }
+    if(d==0)
+    {
+        x1 = -b / (2 * a);
+        printf("%f", x1);
+    }
+    if(d<0)
+    {
+        printf("Уравнение не имеет корней");
+        return 0;
+    }
+}

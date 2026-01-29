081673f9fdc5ada29ff09325590ac47ec68cd79f
Tong228 <KKKostya55@mail.ru>
2026-01-29 21:17:27 +1000

    lab2 in proccess

--- 2/lab2.c
+++ 2/lab2.c
@@ -3,16 +3,16 @@
 int main()
 {
     double matrix3x3[3][3];
-    int n, m, input;
+    int n, m;
 
     for (n=0; n<3; n++)
     {
         for (m=0; m<3; m++)
         {
-            /*scanf("%f", &input);*/
-            matrix3x3[n][m] = scanf();
-            printf("%.0f ", matrix3x3[n][m]);
+            matrix3x3[n][m] = scanf("%lf", &matrix3x3);
+            
         }
+    printf("%.0f ", matrix3x3[n][m]);
     printf("\n");
     }
 }

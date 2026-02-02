e1f9bed26f87ac021ba6c57c54a7d70ae5d12929
Tong228 <KKKostya55@mail.ru>
2026-02-02 21:43:11 +1000

    lab2 in proccess

--- 2/lab2.c
+++ 2/lab2.c
@@ -5,6 +5,7 @@ int main()
     double matrix3x3[3][3];
     int n, m;
 
+    // запись матрицы 
     for (n=0;n<3;n++)
 	{
 		for(m=0;m<3;m++)
@@ -14,17 +15,36 @@ int main()
 		}
 	}
     
-    /*Вывод матрицы*/
+    // /*Вывод матрицы*/
+    // printf("\n");
+    // for (n=0; n<3; n++)
+    // {
+    //     for (m=0; m<3; m++)
+    //     {
+    //         printf("%.0lf ", matrix3x3[n][m]);
+            
+    //     }
+    //     printf("\n");
+    // }
+
+    //Вывод ээлементов главной диагонали
+    double sum = 0;
     for (n=0; n<3; n++)
     {
         for (m=0; m<3; m++)
         {
-            printf("%lf", matrix3x3[n][m]);
-            
+            if(m == n) sum += matrix3x3[n][m];
         }
-
     }
-    getchar();
+    printf("Sum of the elements of the main diagonal: %.0lf\n", sum);
 
+    printf("Press Enter to exits");
+    
+    // Очистка буфера перед ожиданием, если до этого был ввод
+    // fflush(stdin); 
+    getchar(); // Я так понимаю 2 гетчар потому что после ввода последнего числа, я нажимаю Enter, что является /n, гетчар его съедает и завершает программу
+    getchar(); // А когда их два, то следующий гетчар ждёт уже моего решения закрыть программу
+
+    return 0;
 }
 

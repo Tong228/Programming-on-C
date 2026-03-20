27384b59bccf450e8d333a58d27dc2692d01c4c3
Tong228 <KKKostya55@mail.ru>
2026-03-20 22:01:25 +1000

    lab4

---
+++ 4/lab4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b ,c, perimeter;
+    printf("Введите три стороны треугольника: ");
+    if (scanf("%d", &a, &b, &c)) 
+    {
+
+    }
+    perimeter = a + b + c;
+    
+
+    
+}

7e1771ba81f1fde2bdbb618f0e9566e36a1c699b
Tong228 <KKKostya55@mail.ru>
2026-04-11 14:38:30 +1000

    lab5

--- 5/matrix_calc.h
+++ 5/matrix_calc.h
@@ -1,1 +1,3 @@
-void calculate_matrix(double** a, double** b, int n);
+double** allocate_matrix(int n);
+void free_matrix(double** matrix, int n);
+double** calculate_matrix(double** a, double** b, int n, char op);

763e60fec40c2f89b61b9b84b33dec5cfdce3919
Tong228 <KKKostya55@mail.ru>
2026-04-15 20:33:32 +1000

    lab5

--- 5/main.c
+++ 5/main.c
@@ -3,5 +3,9 @@
 
 int main() 
 {
-    
+    double **matrix1 = (double **)malloc(size1 * sizeof(double *));
+    for (int i=0; i < size1; i++)
+    {
+        matrix1[i] = (double *)malloc(size1 * sizeof(double));
+    }
 }

87cbf20e5903260327ab5cefa182fad58ffdb98f
Tong228 <KKKostya55@mail.ru>
2026-04-17 13:19:46 +1000

    lab5

--- 5/matrix_calc.c
+++ 5/matrix_calc.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include "matrix_calc.h"
 
-double** allocate_matrix(int n) {
+double sum_matrix(double a, double b, double res, int n) {
     for (int i = 0; i < n; i++) {
-        
+        for (int j = 0; j < n; j++) {
+            res[i][j] = a[i][j] + b[i][j];
+        }
     }
 }
+

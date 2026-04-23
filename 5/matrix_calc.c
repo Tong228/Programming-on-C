f26146216c3e49232b3d6ad8d0f80f739dde5426
Tong228 <KKKostya55@mail.ru>
2026-04-23 14:07:15 +1000

    lab5

--- 5/matrix_calc.c
+++ 5/matrix_calc.c
@@ -1,32 +1,44 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include "matrix_calc.h"
-
-void sum_matrices(double** a, double** b, double** res, int n) {
+// Функция выделения памяти для матрицы
+double** allocate_matrix(int n) {
+    // Выделяем массив указателей на строки
+    double** matrix = (double**)malloc(n * sizeof(double*));
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            res[i][j] = a[i][j] + b[i][j];
-        }
+        // Для каждой строки выделяем массив элементов
+        matrix[i] = (double*)malloc(n * sizeof(double));
     }
+    return matrix;
+}
+//Функция очистки памяти
+void free_matrix(double** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        //Чистим строку
+        free(matrix[i]); 
+    }
+    //Массив указателей
+    free(matrix); 
 }
 
+double** calculate_matrix(double** a, double** b, int n, char op) {
+    //Выделяем память для результирующего массива
+    double** result = allocate_matrix(n);
 
-void sub_matrices(double** a, double** b, double** res, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            res[i][j] = a[i][j] - b[i][j];
-        }
-    }
-}
-
-void multiply_matrices(double** a, double** b, double** res, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            res[i][j] = 0;
-            for (int k = 0; k < n; k++) {
-                res[i][j] += a[i][k] * b[k][j];
+            if (op == '+') {
+                result[i][j] = a[i][j] + b[i][j];
+            } 
+            else if (op == '-') {
+                result[i][j] = a[i][j] - b[i][j];
+            } 
+            else if (op == '*') {
+                result[i][j] = 0;
+                for (int k = 0; k < n; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
             }
         }
     }
+    return result;
 }

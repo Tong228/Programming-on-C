d1364980343bef50283122db7ad31fac8483909f
unknown <KKKostya55@mail.ru>
2026-05-08 18:36:55 +1000

    lab8

---
+++ 8/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Описываем структуру человека
+typedef struct {
+    char name[50];
+    char surname[50];
+    int year;
+    char gender[10];
+    float height;
+} Person;
+
+int comparison () {
+    for(int i = 0) {
+
+    }
+}
